96f1013eacda76ffbe45b054365aa7ed02298a29
Oswaldo Cevallos <cevallos.oswaldo@gmail.com>
2025-09-05 10:33:28 -0500

    ueva version

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "Keapp",
   slug: "keapp",
   scheme: "kestoreec",
-  version: "1.0.9", // <- Versión actualizada con mejoras
+  version: "1.0.10", // <- Versión actualizada con mejoras
   orientation: "default",
   userInterfaceStyle: "automatic",
   owner: "wm-notificaciones",
@@ -27,7 +27,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   // Si prefieres no actualizar esto a mano, puedes usar:
   // runtimeVersion: { policy: "appVersion" },
-  runtimeVersion: "1.0.9",
+  runtimeVersion: "1.0.10",
 
   extra: {
     eas: { projectId: "d1ba3c41-db4d-4c80-95a3-1a18dfa35aab" },
@@ -42,7 +42,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   // ====== ANDROID ONLY ======
   android: {
     package: "com.ke.ecuadorv2",
-    versionCode: 15, // <- Incrementado para nueva versión
+    versionCode: 16, // <- Incrementado para nueva versión
     adaptiveIcon: {
       foregroundImage: "./assets/images/adaptive-icon.png",
       backgroundColor: "#000000",
